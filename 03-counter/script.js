5106f428c06b81c36dc681bb3479eaf75b557005
yumin <102840734+miyen16@users.noreply.github.com>
2022-04-29 00:53:57 +0800

    Add files via upload

---
+++ 03-counter/script.js
@@ -0,0 +1,41 @@
+// const decrease = document.getElementById('decrease');
+// const resrt = document.getElementById('resrt');
+// const increase = document.getElementById('increase');
+
+const number = document.querySelector('.number');
+const btns = document.querySelectorAll('.btn');
+// console.log(btns);
+
+//classList獲得class
+//currentTarget抓取當前目標資料
+// btns.forEach(function (btn) {
+//     btn.addEventListener('click', function (e) {
+//         console.log(e.currentTarget.classList);
+//     });
+// // console.log(btn);
+// });
+
+//contains('')包含括弧內的文字時
+let count = 0;
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count > 0) {
+            number.style.color = 'green';
+        }
+        if (count < 0) {
+            number.style.color = 'red';
+        }
+        if (count === 0) {
+            number.style.color = 'rgb(125, 70, 136)';
+        }
+        number.textContent = count;
+    });
+});
